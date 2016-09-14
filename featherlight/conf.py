b79479928fe0120717523a4f1410fcc3593a74d5
JP Viljoen <froztbyte@froztbyte.net>
2016-09-14 15:24:15 +0200

    [site] change org url

--- featherlight/conf.py
+++ featherlight/conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "JP Viljoen"  # (translatable)
 BLOG_TITLE = "Featherlight Consulting"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://featherlight.github.io/"
+SITE_URL = "https://featherlight-org.github.io/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://featherlight.github.io/"
